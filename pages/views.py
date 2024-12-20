ded1c5da716f3d1b4dce0017c2c8de1d159285e6
Jim McKeown <mckeown.james@gmail.com>
2024-12-20 18:48:27 -0500

    added views, method, search

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+from listings.models import Listing
+from realtors.models import Realtor
+from listings import choices
+
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.all()[:3]
+
+    context = {
+        "listings": listings,
+        'bedroom_choices': choices.bedroom_choices,
+        'state_choices': choices.state_choices,
+        'price_choices': choices.price_choices
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.all()
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        "realtors": realtors,
+        "mvp_realtors": mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
